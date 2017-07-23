8d00bddc2e6678781809e816397e7f5552fc432b
Lucas Narciso <lucasnar@gmail.com>
2017-07-23 13:57:41 -0300

    Create app.js
    - Initiate it with a simple echo bot example.

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+const http = require('http');
+const Bot = require('messenger-bot');
+
+let bot = new Bot({
+  token: process.env.PAGE_TOKEN,
+  verify: process.env.VERIFY_TOKEN
+});
+
+bot.on('error', (err) => {
+    console.log(err.message)
+});
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text;
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
+
+    reply({ text }, (err) => {
+      if (err) throw err;
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    });
+  });
+});
+
+// SERVER
+http.createServer(bot.middleware()).listen(4500);
+console.log('Echo bot server running at port 4500.');
