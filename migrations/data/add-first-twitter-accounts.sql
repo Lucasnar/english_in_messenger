b5904b76957d8ae3c5b4b898088fda9edd0a1ad2
Lucasnar <lucasnar@gmail.com>
2018-01-09 23:08:10 -0200

    Remove the db-migrate dependency
    - Just found out about Sequelize

--- migrations/data/add-first-twitter-accounts.sql
+++
@@ -1,4 +0,0 @@
-/* I perform this delete first to simplify not inserting duplicate values in development */
-DELETE FROM twitter_accounts WHERE screen_name = 'bbcworld' OR screen_name = 'reuters' OR screen_name = 'wsj' OR screen_name = 'guardian' OR screen_name = 'theeconomist' OR screen_name = 'npr';
-
-INSERT INTO twitter_accounts (name, screen_name) VALUES ('BBC World', 'bbcworld'), ('Reuters', 'reuters'), ('Wall Street Journal', 'wsj'), ('The Guardian', 'guardian'), ('The Economist', 'theeconomist'), ('NPR', 'npr');
