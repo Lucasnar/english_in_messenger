715cff590fea74377b5950b67c958b27786e580b
Lucas Narciso <lucasnar@gmail.com>
2018-03-30 17:40:25 -0300

    Use one central config to Sequelize
    There is no need to separate the cli's config from the app's.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
 module.exports = {
+  "define": {
+    "underscored": true,
+    "timestamps": true,
+    "createdAt": "created_at",
+    "updatedAt": "updated_at",
+    "deletedAt": "deleted_at",
+  },
   "development": {
     "username": process.env.PGUSER,
     "password": process.env.PGPASSWORD,
     "database": process.env.PGDATABASE,
     "hostname": process.env.PGHOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
   }
 }
