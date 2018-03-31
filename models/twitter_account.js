d2f2bc9f862079cc1c433fe0941bc0f5f9ae9b78
Lucas Narciso <lucasnar@gmail.com>
2018-03-31 10:57:10 -0300

    Rename models' names to be singular

--- models/twitter_account.js
+++ models/twitter_account.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var TwitterAccounts = sequelize.define('twitter_accounts', {
+  var TwitterAccount = sequelize.define('twitter_accounts', {
     name: DataTypes.STRING,
     screenName: { type: DataTypes.STRING, field: 'screen_name' },
     twitterid: DataTypes.STRING
   });
 
-  TwitterAccounts.associate = function (models) {
+  TwitterAccount.associate = function (models) {
     // associations can be defined here
   };
 
-  return TwitterAccounts;
+  return TwitterAccount;
 };
