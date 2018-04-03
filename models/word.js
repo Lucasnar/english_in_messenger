3c54e40190957e8de098cdb1121ce335d5d8eea4
Lucas Narciso <lucasnar@gmail.com>
2018-04-03 01:31:38 -0300

    Change image_url to be camel case in word model
    Even though the column name is image_url, the variable name should be
    imageUrl to better follow Javascript conventions.

--- models/word.js
+++ models/word.js
@@ -2,12 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   var Word = sequelize.define('words', {
     word: DataTypes.STRING,
-    image_url: DataTypes.STRING
+    imageUrl: { type: DataTypes.STRING, field: 'image_url' }
   }, {
     underscored: true,
   });
 
-  Word.associate =  function (models) {
+  Word.associate = function (models) {
     Word.belongsTo(models.Language);
   };
 
