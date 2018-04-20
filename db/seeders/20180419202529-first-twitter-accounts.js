21019a32f91f1452443cfb61f5a5b538104e8646
Lucas Narciso <lucasnar@gmail.com>
2018-04-20 13:46:32 -0300

    Add language in twitter accounts seeder
    The language for those accounts is English.

--- db/seeders/20180419202529-first-twitter-accounts.js
+++ db/seeders/20180419202529-first-twitter-accounts.js
@@ -1,38 +1,51 @@
 'use strict';
 
+const db = require('../../models/index.js');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('twitter_accounts', [{
-      name: 'BBC World',
-      screen_name: 'bbcworld',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      name: 'Reuters',
-      screen_name: 'reuters',
-      created_at: new Date(),
-      updated_at: new Date()
-    },{
-      name: 'Wall Street Journal',
-      screen_name: 'wsj',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      name: 'The Guardian',
-      screen_name: 'guardian',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      name: 'The Economist',
-      screen_name: 'theeconomist',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      name: 'NPR',
-      screen_name: 'npr',
-      created_at: new Date(),
-      updated_at: new Date()
-    } ], { logging: console.log });
+    return db.Language.find({
+      where: { name: 'English' }
+    }).then(function(english) {
+      let englishId = english.dataValues.id
+      queryInterface.bulkInsert('twitter_accounts', [{
+        name: 'BBC World',
+        screen_name: 'bbcworld',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        name: 'Reuters',
+        screen_name: 'reuters',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      },{
+        name: 'Wall Street Journal',
+        screen_name: 'wsj',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        name: 'The Guardian',
+        screen_name: 'guardian',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        name: 'The Economist',
+        screen_name: 'theeconomist',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, {
+        name: 'NPR',
+        screen_name: 'npr',
+        language_id: englishId,
+        created_at: new Date(),
+        updated_at: new Date()
+      } ], { logging: console.log });
+    });
   },
 
   down: (queryInterface, Sequelize) => {
